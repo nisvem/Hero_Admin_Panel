98771bd100f3ce745e76d45f5cc3f5dedb50ee58
nisvem <nisvem@gmail.com>
2024-04-09 11:46:15 +0200

    Initial commit

---
+++ src/hooks/http.hook.js
@@ -0,0 +1,43 @@
+import { useCallback, useState } from 'react';
+import axios from 'axios';
+
+export const useHttp = () => {
+  const [process, setProcess] = useState('waiting');
+
+  const request = useCallback(
+    async (
+      url,
+      method = 'GET',
+      body = null,
+      headers = { 'Content-Type': 'application/json' }
+    ) => {
+      setProcess('loading');
+
+      try {
+        const response = await axios({
+          method: method,
+          url: url,
+          data: body,
+          headers: headers,
+        });
+
+        return response.data;
+      } catch (e) {
+        setProcess('error');
+        throw new Error(`Could not fetch ${url}, status: ${e}`);
+      }
+    },
+    []
+  );
+
+  // const clearError = useCallback(() => {
+  // setProcess('loading');
+  // }, []);
+
+  return {
+    request,
+    // clearError,
+    process,
+    // setProcess
+  };
+};
